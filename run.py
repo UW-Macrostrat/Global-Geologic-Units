6e4cc9155f2133d6ba8f016c5831b50f8885e0e6
Ian Ross <ian.ackerman.ross@gmail.com>
2016-03-02 12:03:26 -0600

    Add example app; add pyyaml to requirements; move sample data to input

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
+"""
+File: run.py
+Description: Example app utilizing the GeoDeepDive infrastructure and products.
+    This will look at the produced NLP table and print a list of proper nouns which
+    are modified by adjectives, along with the sentence id in which they occur.
+Assumes: make setup-local has been run (so that the example database is populated)
+"""
+
 import yaml
 import psycopg2
 from psycopg2.extensions import AsIs
@@ -15,3 +23,35 @@ connection = psycopg2.connect(
     host=credentials['postgres']['host'],
     port=credentials['postgres']['port'])
 cursor = connection.cursor()
+
+proper_nouns_with_adj = {} # key: proper_noun, value: (adjective, sentence_id)
+
+# read all sentences from our NLP example database.
+cursor.execute("SELECT * FROM stringed_instruments_sentences_nlp352;")
+sentences = cursor.fetchall()
+for sentence in sentences:
+    sentid = sentence[1]
+    words = sentence[3]
+    poses = sentence[4]
+    dep_parents = sentence[8]
+    proper_nouns = [] # list of proper nouns
+    adjectives = [] # list of adjectives
+    for idx, pos in enumerate(poses): # look for proper nouns and adjectives
+        if pos=="NNP":
+            proper_nouns.append(idx)
+        elif pos=="JJ":
+            adjectives.append(idx)
+    for idx, parent in enumerate(dep_parents): # loop over dependencies to look for adjectives which relate to a proper noun
+        # within the table, the dep_parents is indexed from 1.  Our internal
+        # indexing is from 0, so subtract one.
+        if idx in adjectives and parent-1 in proper_nouns:
+            if words[parent-1] in proper_nouns_with_adj:
+                proper_nouns_with_adj[words[parent-1]].append((words[idx], sentid))
+            else:
+                proper_nouns_with_adj[words[parent-1]] = [(words[idx], sentid)]
+
+# write results to the output directory
+with open("./output/proper_nouns_with_adjectives", "w") as fout:
+    for proper_noun in proper_nouns_with_adj.keys():
+        fout.write("%s - %s\n" % (proper_noun, proper_nouns_with_adj[proper_noun]))
+
--- setup/setup.sh
+++ setup/setup.sh
@@ -27,14 +27,14 @@ createdb $postgres__database -h $postgres__host -U $postgres__user -p $postgres_
 # Vanilla NLP
 echo "DROP TABLE IF EXISTS ${app_name}_sentences_nlp; CREATE TABLE ${app_name}_sentences_nlp (docid text, sentid integer, wordidx integer[], words text[], poses text[], ners text[], lemmas text[], dep_paths text[], dep_parents integer[], font text[], layout text[]);" | psql -U $postgres__user -h $postgres__host -p $postgres__port $postgres__database
 
-echo "COPY ${app_name}_sentences_nlp FROM '$pwd/setup/usgs_example/sentences_nlp'" | psql -U $postgres__user -h $postgres__host -p $postgres__port $postgres__database
+echo "COPY ${app_name}_sentences_nlp FROM '$pwd/input/sentences_nlp'" | psql -U $postgres__user -h $postgres__host -p $postgres__port $postgres__database
 
 # NLP352
 echo "DROP TABLE IF EXISTS ${app_name}_sentences_nlp352; CREATE TABLE ${app_name}_sentences_nlp352 (docid text, sentid integer, wordidx integer[], words text[], poses text[], ners text[], lemmas text[], dep_paths text[], dep_parents integer[]);" | psql -U $postgres__user -h $postgres__host -p $postgres__port $postgres__database
 
-echo "COPY ${app_name}_sentences_nlp352 FROM '$pwd/setup/usgs_example/sentences_nlp352'" | psql -U $postgres__user -h $postgres__host -p $postgres__port $postgres__database
+echo "COPY ${app_name}_sentences_nlp352 FROM '$pwd/input/sentences_nlp352'" | psql -U $postgres__user -h $postgres__host -p $postgres__port $postgres__database
 
 # NLP352 Bazaar
 echo "DROP TABLE IF EXISTS ${app_name}_sentences_nlp352_bazaar; CREATE TABLE ${app_name}_sentences_nlp352_bazaar (docid text, sentid integer, sentence text, words text[], lemmas text[], poses text[], ners text[], character_position integer[], dep_paths text[], dep_parents integer[]);" | psql -U $postgres__user -h $postgres__host -p $postgres__port $postgres__database
 
-echo "COPY ${app_name}_sentences_nlp352_bazaar FROM '$pwd/setup/usgs_example/sentences_nlp352_bazaar'" | psql -U $postgres__user -h $postgres__host -p $postgres__port $postgres__database
+echo "COPY ${app_name}_sentences_nlp352_bazaar FROM '$pwd/input/sentences_nlp352_bazaar'" | psql -U $postgres__user -h $postgres__host -p $postgres__port $postgres__database
