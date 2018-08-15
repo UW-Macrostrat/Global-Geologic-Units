63d63193d5d147534e3c45a8dc4cb88988bfedf0
Daven Quinn <dev@davenquinn.com>
2018-08-15 02:35:04 -0500

    Added parent references

--- model_implementation/sentence.py
+++ model_implementation/sentence.py
@@ -5,12 +5,39 @@ Description: A set of classes to represent an NLP352-encoded sentence
 """
 
 class Word(object):
+    """
+    This class should not be initialized directly as it depends
+    on the keyword arguments being set correctly
+    """
     def __init__(self, **kw):
-        self.text = kw.pop('text')
-        self.sentence = kw.pop('sentence', None)
+        for k,v in kw.items():
+            setattr(self, k,v)
+        # Correct for 1-indexing of parent data
+        if self.dep_parent == 0:
+            self.dep_parent = None
+        else:
+            self.dep_parent -= 1
 
     def __str__(self):
         return self.text
+
+    def __repr__(self):
+        return f"{self.text}: {self.pose}, {self.ner}"
+
+    @property
+    def parent(self):
+        if self.dep_parent is None:
+            return None
+        return self.sentence.words[self.dep_parent]
+
+    @property
+    def tree(self):
+        p = self.parent
+        txt = str(self)
+        while p is not None:
+            txt += " → "+str(p)
+            p = p.parent
+        return txt
 
 class Sentence(object):
     def __init__(self, sentence):
@@ -19,11 +46,20 @@ class Sentence(object):
         representing a single sentence.
         """
         self.__raw__ = sentence
+        r = self.__raw__
         self.words = []
         for i, word in enumerate(sentence.words):
             w = Word(
                 text=word,
-                sentence=self)
+                sentence=self,
+                # We're doing zero-indexing here, despite 1-indexing of parent data
+                index=i,
+                pose=r.poses[i],
+                ner=r.ners[i],
+                lemma=r.lemmas[i],
+                dep_path=r.dep_paths[i],
+                dep_parent=r.dep_parents[i]
+                )
             self.words.append(w)
 
     def __str__(self):
@@ -40,4 +76,14 @@ class Sentence(object):
         for s in subs:
             joined=joined.replace(*s)
         return joined
+
+    def print_breakdown(self):
+        for word in self.words:
+            print(repr(word))
+
+    def __iter__(self):
+        return (s for s in self.words)
+
+    def __getitem__(self, ix):
+        return self.words[ix]
 
