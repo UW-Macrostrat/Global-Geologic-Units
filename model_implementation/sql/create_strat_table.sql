78bb3ffce16dcbc8e3c26f2b4f97536f5e240497
[Julia Wilcots] <[jwilcots@mit.edu]>
2018-08-22 17:40:15 -0400

    sql tables for macrostrat data

---
+++ model_implementation/sql/create_strat_table.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS macrostrat_strat;
+CREATE TABLE macrostrat_strat (
+  strat_name text,
+  strat_name_long text,
+  rank text,
+  strat_name_id numeric PRIMARY KEY,
+  concept_id numeric,
+  bed text,
+  bed_id text,
+  mbr text,
+  mbr_id text,
+  fm text,
+  fm_id text,
+  subgp text,
+  subgp_id text,
+  b_age numeric,
+  t_age numberic,
+  b_period text,
+  t_period text,
+  c_interval text,
+  t_units text,
+  ref_id numeric
+);
+COPY macrostrat_strat FROM :filename WITH (HEADER, FORMAT 'csv');
